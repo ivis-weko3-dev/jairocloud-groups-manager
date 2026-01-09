9cbdea58bdebada598eb4b411b48454eb0b8e9b3
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2026-01-09 11:18:31 +0000

    docs: update constant definitions

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
         highlight: {
           theme: {
             default: 'github-light',
+            light: 'github-light',
             dark: 'github-dark',
           },
           langs: ['python'],
