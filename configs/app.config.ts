8df1e5fd81309c2d97019702dbbdf13a1daec368
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2026-02-27 12:10:43 +0000

    fix(config): update to latest values
    - shorten the pattern for the group ID
    -fix spelling mistakes

--- configs/app.config.ts
+++ configs/app.config.ts
@@ -55,7 +55,7 @@ const repositories = {
    * in `server.config.toml` accordingly to keep them consistent.
    * [Mandatory]
    */
-  maxUrlLength: 50 - 'https://'.length - 'jc_'.length - 9,
+  maxUrlLength: 50 - 'jc_'.length - '_ro_radm'.length - 2,
 }
 
 const groups = {
@@ -64,7 +64,7 @@ const groups = {
    * (excluding the “jc_” prefix assigned to the ID and additional padding) \
    * [Mandatory]
    */
-  maxIdLength: 50 - 'jc_'.length - '_groups_'.length - 4,
+  maxIdLength: 50 - 'jc_'.length - '_gr_'.length,
 }
 
 const wayf = {
